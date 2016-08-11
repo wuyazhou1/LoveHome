2212b05364ceae6e785c3e42a78679d3ee8b833c
yinjianlin <465834228@qq.com>
2016-08-11 17:33:26 +0800

    修改

--- minelibrary/src/main/java/com/example/administrator/minelibrary/mineFrament.java
+++ minelibrary/src/main/java/com/example/administrator/minelibrary/mineFrament.java
@@ -8,15 +8,15 @@ import android.view.View;
 import android.view.ViewGroup;
 
 /**
- * Created by Administrator on 2016/8/11.
- */
-public class mineFrament extends Fragment {
-    @Override
-    public View onCreateView(final LayoutInflater inflater,
-                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view =  inflater.inflate(R.layout.mine_fragment, null);
+        * Created by Administrator on 2016/8/11.
+                */
+        public class mineFrament extends Fragment {
+            @Override
+            public View onCreateView(final LayoutInflater inflater,
+                                     @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+                View view =  inflater.inflate(R.layout.mine_fragment, null);
 
-        return view;
+                return view;
 
 
     }
