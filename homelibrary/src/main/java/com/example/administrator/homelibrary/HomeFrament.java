5975e65bd70b3bd91261bcae7876d96b1de5d2d8
wuyazhou1 <1720473619@qq.com>
2016-08-11 15:00:09 +0800

    Initial commit

---
+++ homelibrary/src/main/java/com/example/administrator/homelibrary/HomeFrament.java
@@ -0,0 +1,23 @@
+package com.example.administrator.homelibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/8/11.
+ */
+public class HomeFrament extends Fragment {
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.home_fragment, null);
+
+        return view;
+
+
+    }
+}
---
+++ publishlibrary/src/main/java/com/example/administrator/publishlibrary/PublishFrament.java
@@ -0,0 +1,24 @@
+package com.example.administrator.publishlibrary;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Administrator on 2016/8/11.
+ */
+public class PublishFrament extends Fragment {
+
+    @Override
+    public View onCreateView(final LayoutInflater inflater,
+                             @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view =  inflater.inflate(R.layout.publish_fragment, null);
+
+        return view;
+
+
+    }
+}
